226fb3fa193aa67d1b1656d25acb697457a3758f
atsuage <atsuage.programmer@gmail.com>
2021-08-30 16:55:14 +0900

    Add file for heroku

---
+++ MyBBS/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '6c1158898ee8dfefc9e8ff4f5a3e101401e40333',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '6c1158898ee8dfefc9e8ff4f5a3e101401e40333',
+            'dev_requirement' => false,
+        ),
+    ),
+);
