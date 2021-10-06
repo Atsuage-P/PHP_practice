adff2fbffe839dcd4b2ae8d55be59e86cc4cc53e
atsuage <atsuage.programmer@gmail.com>
2021-10-06 11:20:01 +0900

    Modified timezone

---
+++ Laravel_practice/resources/views/hello/index.php
@@ -0,0 +1,5 @@
+<body>
+    <h1>Hello, World!</h1>
+    <p><?= $msg; ?></p>
+    <p><?= date("Y年n月j日") ?></p>
+</body>
--- MyBBS/work/app/config.php
+++ MyBBS/work/app/config.php
@@ -3,18 +3,18 @@
 session_start();
 
 // ローカル環境
-// define('DSN', 'mysql:host=db;dbname=myapp;charset=utf8mb4');
-// define('DB_USER', 'myappuser');
-// define('DB_PASS', 'myapppass');
-// define('SITE_URL', 'http://' . $_SERVER['HTTP_HOST']);
+define('DSN', 'mysql:host=db;dbname=myapp;charset=utf8mb4');
+define('DB_USER', 'myappuser');
+define('DB_PASS', 'myapppass');
+define('SITE_URL', 'http://' . $_SERVER['HTTP_HOST']);
 
 // 本番環境
-define('DSN', 'mysql:host=us-cdbr-east-04.cleardb.com;dbname=heroku_18598fa8a1b2ba9;charset=utf8mb4');
-define('DB_NAME', 'heroku_18598fa8a1b2ba9');
-define('DB_HOST', 'us-cdbr-east-04.cleardb.com');
-define('DB_USER', 'b298720dc1c0ee');
-define('DB_PASS', '388b13ff');
-define('SITE_URL', 'http://' . $_SERVER['HTTP_HOST']);
+// define('DSN', 'mysql:host=us-cdbr-east-04.cleardb.com;dbname=heroku_18598fa8a1b2ba9;charset=utf8mb4');
+// define('DB_NAME', 'heroku_18598fa8a1b2ba9');
+// define('DB_HOST', 'us-cdbr-east-04.cleardb.com');
+// define('DB_USER', 'b298720dc1c0ee');
+// define('DB_PASS', '388b13ff');
+// define('SITE_URL', 'http://' . $_SERVER['HTTP_HOST']);
 
 require_once(__DIR__ . '/Utils.php');
 require_once(__DIR__ . '/Token.php');
